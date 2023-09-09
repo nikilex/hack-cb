efb8f6160d2552e95f90a12968decc6d9149ef54
Алексей Никитин <a.nikitin@k-telecom.org>
2023-09-10 02:51:06 +0500

    Связал с апишкой

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 
 import { useState, useEffect } from "react";
 
+import Button from 'react-bootstrap/Button';
+
 import Image from "next/image";
 import styles from "./page.module.css";
 import TextField from "@mui/material/TextField";
@@ -21,13 +23,41 @@ function randomInteger(min: number, max: number): string {
   return Math.floor(rand).toString();
 }
 
+interface Predict {
+  prediction1: string;
+  prediction2: string;
+}
+
 export default function Home() {
   const data = getData();
+
+  const [predict, setPredict] = useState<Predict>();
+  const [isLoading, setLoading] = useState(true);
+
+  const [text, setText] = useState(data.report);
+
+  const [file, setFile] = useState();
 
   const [randNum, setRandNum] = useState("");
 
   useEffect(() => {
     setRandNum(randomInteger(0, 4).toString());
+    var header = new Headers();
+
+    // Your server does not currently allow this one
+    header.append("Content-Type", "application/json;charset=utf-8");
+
+    fetch("http://80.87.109.214:5000/predict_text", {
+      method: "POST",
+      // mode: "no-cors",
+      // headers: header,
+      body: JSON.stringify({ text: data.report }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setPredict(data);
+        setLoading(false);
+      });
   }, []);
 
   const renderListOfArray = (items: string[]) => {
@@ -70,14 +100,56 @@ export default function Home() {
     },
   ];
 
+  const handleClick = async () => {
+    setLoading(true);
+
+    await fetch("http://80.87.109.214:5000/predict_text", {
+      method: "POST",
+      // mode: "no-cors",
+      // headers: header,
+      body: JSON.stringify({ text: text }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setPredict(data);
+        setLoading(false);
+      });
+  }
+
+  const sendFile = async (e: any) => {
+    setLoading(true);
+    let files = e.target.files;
+
+    setFile(files[0]);
+
+    const formData = new FormData();
+    formData.append("file", files[0], files[0].name);
+
+    await fetch("http://80.87.109.214:5000/upload", {
+      method: "POST",
+      // mode: "no-cors",
+      // headers: header,
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setPredict(data);
+        setLoading(false);
+      });
+  }
+
   return (
     <main className={styles.main}>
       <Container className="p-5">
         <Row className="mb-3">
           <Col className="bordered">
             <h3 className="mb-0">
-              {data.company}{" "}
-              <span style={{ color: "green" }}>{data.rating}</span>
+              <Row className="justify-content-between">
+                {/* <Col xs="4">{data.company}</Col> */}
+                <Col xs="4" >Рейтинг: <span style={{ color: "green" }}>{predict ? predict.prediction1[0] : ''}</span></Col>
+              </Row>
+              
+              
             </h3>
           </Col>
         </Row>
@@ -99,13 +171,22 @@ export default function Home() {
                     <Form.Control
                       as="textarea"
                       rows={19}
-                      defaultValue={data.report}
+                      value={text}
+                      onChange={e => setText(e.target.value)}
                     ></Form.Control>
+                    <Button variant="primary"
+                    disabled={isLoading}
+                    onClick={handleClick}
+                    >Отправить</Button>
                   </Tab>
                   <Tab eventKey="file" title="Файл">
                     <Form.Group controlId="formFileLg" className="mb-3">
                       <Form.Label>Выберите файл</Form.Label>
-                      <Form.Control type="file" size="lg" />
+                      <Form.Control type="file" size="lg" value={file} onChange={e => setText(e.target.value)}/>
+                      <Button variant="primary"
+                    disabled={isLoading}
+                    onClick={sendFile}
+                    >Отправить</Button>
                     </Form.Group>
                   </Tab>
                 </Tabs>
@@ -150,7 +231,8 @@ export default function Home() {
 }
 
 function getData() {
-  //const res = await fetch('https://api.example.com/...')
+  // const res = await fetch('http://80.87.109.214:5000/test')
+  // console.log(res.json());
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
@@ -161,7 +243,7 @@ function getData() {
     metricas: string[];
     report: string;
   } = {
-    rating: "A++",
+    rating: "AAA",
     company: "ПАО Сбербанк",
     criterias: [
       "Выручка",
